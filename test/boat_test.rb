a649b00faba00df8000bf522707a4373d69aac39
Kelly Sandeson <kellysandeson@gmail.com>
2019-05-18 23:29:43 -0600

    Create successful boat tests for iteration 1.

---
+++ test/boat_test.rb
@@ -0,0 +1,38 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/boat'
+require './lib/renter'
+
+class BoatTest < Minitest::Test
+
+  def test_it_exists
+    kayak = Boat.new(:kayak, 20)
+    assert_instance_of Boat, kayak
+  end
+
+  def test_it_has_attributes
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal :kayak, kayak.type
+    assert_equal 20, kayak.price_per_hour
+  end
+
+  def test_hours_rented
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_add_hour
+    kayak = Boat.new(:kayak, 20)
+
+    kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+    kayak.hours_rented
+
+    assert_equal 3 , kayak.hours_rented
+  end
+
+end
