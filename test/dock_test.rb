79cfaeb9b59dd47a9ea6db052b772ab0298e641a
Kelly Sandeson <kellysandeson@gmail.com>
2019-05-19 14:55:20 -0600

    Complete dock_test, no errors or failures. Iteration 2 requirements complete.

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -2,8 +2,8 @@ require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/boat'
-require './lib/renter'
 require './lib/dock'
+require './lib/renter'
 
 class DockTest < Minitest::Test
 
@@ -18,19 +18,24 @@ class DockTest < Minitest::Test
     assert_equal 3, dock.max_rental_time
   end
 
-  # def test_the_docks_rental_log
-  #   dock = Dock.new("The Rowing Dock", 3)
-  #
-  #   kayak_1 = Boat.new(:kayak, 20)
-  #   # kayak_2 = Boat.new(:kayak, 20)
-  #
-  #   patrick = Renter.new("Patrick Star", "4242424242424242")
-  #   # eugene = Renter.new("Eugene Crabs", "1313131313131313")
-  #
-  #   dock.rent(kayak_1, patrick)
-  #   # dock.rent(kayak_2, eugine)
-  #
-  #   assert_equal {flim: flam}, dock.rental_log
-  # end
+  def test_the_docks_rental_log
+    dock = Dock.new("The Rowing Dock", 3)
+
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    expected = { kayak_1 => patrick,
+                 kayak_2 => patrick,
+                  sup_1 => eugene}
+
+    assert_equal expected, dock.rental_log
+  end
 
 end
